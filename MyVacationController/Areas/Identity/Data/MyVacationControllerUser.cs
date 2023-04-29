0adfd094d09a63ad30e725017e8c3e03e41d4044
Björn Agnemo <bjorn.agnemo@gmail.com>
2023-04-29 23:58:14 +0200

    update: add name props to user class

--- MyVacationController/Areas/Identity/Data/MyVacationControllerUser.cs
+++ MyVacationController/Areas/Identity/Data/MyVacationControllerUser.cs
@@ -9,5 +9,9 @@ namespace MyVacationController.Areas.Identity.Data;
 // Add profile data for application users by adding properties to the MyVacationControllerUser class
 public class MyVacationControllerUser : IdentityUser
 {
-}
+    [PersonalData]
+    public string FirstName { get; set; } = string.Empty;
 
+    [PersonalData]
+    public string LastName { get; set; } = string.Empty;
+}
