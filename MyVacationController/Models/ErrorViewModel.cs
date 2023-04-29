b279d6fbcf57891903184c74b1705ae3c5be8cae
Björn Agnemo <bjorn.agnemo@gmail.com>
2023-04-29 21:09:55 +0200

    chore: initial commit

---
+++ MyVacationController/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace MyVacationController.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
